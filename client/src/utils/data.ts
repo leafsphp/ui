471e39034d9a72bf093a9139a3bb7855859919e0
mychidarko <mickdd22@gmail.com>
2023-01-31 23:26:32 +0000

    feat: add regex operations

---
+++ client/src/utils/data.ts
@@ -0,0 +1,20 @@
+export const eventDirectivePrefixRE = (): RegExp => /on|@/gim;
+export const rawDirectiveSplitRE = (): RegExp => /:|\./gim;
+
+export const hasDirectiveRE = (): RegExp => {
+    return new RegExp(
+        `(ui-|${Object.keys(DIRECTIVE_SHORTHANDS).join('|')})\\w+`,
+        'gim'
+    );
+};
+
+export const expressionPropRE = (prop: string): RegExp => {
+    // Utilizes \b (word boundary) for prop differentiation.
+    // Fails when next character is a \w (Word).
+    return new RegExp(`\\b${prop}\\b`, 'gim');
+};
+
+export enum DIRECTIVE_SHORTHANDS {
+    '@' = 'on',
+    ':' = 'bind'
+}
