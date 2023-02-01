9ca19ff2e4e6293f1bb2b53c139ee7a8470ed620
mychidarko <mickdd22@gmail.com>
2023-02-01 22:31:57 +0000

    feat: add lazy util

---
+++ client/src/utils/lazy.ts
@@ -0,0 +1,24 @@
+// Lazy allows us to delay render calls if the main thread is blocked
+// This is kind of like time slicing in React but less advanced
+// It's a generator function that yields after a certain amount of time
+// This allows the browser to render other things while the generator is running
+// It's a bit like a setTimeout but it's more accurate
+
+export const lazy = (
+  threshold: number,
+  generatorFunction: () => Generator<undefined, void, unknown>,
+): (() => void) => {
+  const generator = generatorFunction();
+  return function next() {
+    const start = performance.now();
+    let task = null;
+    do {
+      task = generator.next();
+    } while (performance.now() - start < threshold && !task.done);
+
+    if (task.done) return;
+    setTimeout(next);
+  };
+};
+
+export default lazy;
