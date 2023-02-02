69b284631d3dc4bd2615c6a6224735f0d9d9489d
mychidarko <mickdd22@gmail.com>
2023-02-02 22:35:49 +0000

    feat: add arraysMatch helper

--- client/src/utils/data.ts
+++ client/src/utils/data.ts
@@ -22,6 +22,15 @@ export enum DIRECTIVE_SHORTHANDS {
     ':' = 'bind'
 }
 
+export function arraysMatch(a: any[], b: any[]) {
+    return (
+        Array.isArray(a) &&
+        Array.isArray(b) &&
+        a.length === b.length &&
+        a.every((val, index) => val === b[index])
+    );
+}
+
 declare global {
     interface Window {
         leafUI: Component;
