ee1c5ddbcb6bdf5459c3be62033367edc2c2cf04
mychidarko <mickdd22@gmail.com>
2023-02-11 14:13:36 +0000

    feat: add component key

--- src/UI/Component.php
+++ src/UI/Component.php
@@ -5,6 +5,11 @@ namespace Leaf\UI;
 class Component
 {
     /**
+     * @var string $key The unique key of the component
+     */
+    public $key;
+
+    /**
      * @return string
      */
     public function render()
